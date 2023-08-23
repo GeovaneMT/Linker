3cb88c89da708e94df9606042b16a22a33089fae
GeovaneMT <103201712+GeovaneMT@users.noreply.github.com>
2023-08-22 22:35:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const video = document.getElementById('video')
+const youtube = document.getElementById('youtube')
+const overlay = document.getElementById('overlay')
+
+function popup() {
+    youtube.classList.toggle("show");
+    overlay.classList.toggle("show");
+}
